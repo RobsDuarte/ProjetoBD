4137adf86cf057adb4aeb50cd69d44854ef49d88
Robson Duarte dos Santos <robson_jb_rj@hotmail.com>
2020-11-08 17:15:07 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">        
+        <link href="style.css" rel="stylesheet" type="text/css">    
+    </head>
+        
+    <body>
+
+        <nav class="col-12 header">
+            <span class="negrito">Universidade Federal do Rio de janeiro- UFRJ</span><br>
+            <span class="apresentacao">Trabalho Banco de Dados 1</span> <br>
+            <span class="apresentacao">Interface para mostrar buscas no banco de dados</span><br> 
+            <span class="apresentacao">Criadores:Marcelo Campanelli, Ronald Albert,Robson Duarte dos Santos</span>       
+        </nav>
+
+        <div class='row '>
+            <div class="col-1"></div>
+
+            <div class="col-3 info-box border ">
+                <div class="col-2 name-search">Busca 1</div>
+            </div>
+
+            <div class="col-1"></div>
+
+            <div class="col-3 info-box border">
+                <div class="col-2 name-search">Busca 2</div>
+            </div>
+
+            <div class="col-1"></div>
+
+            <div class="col-3 info-box border ">
+                <div class="col-2 name-search ">Busca 3</div>
+            </div>
+
+            
+
+            <div class="col-12"></div>
+
+            <div class="col-1"></div>
+
+            <div class="col-3 info-box border">
+                <div class="col-2 name-search">Busca 4</div>
+            </div>
+
+            <div class="col-1"></div>
+
+            <div class="col-3 info-box border">
+                <div class="col-2 name-search">Busca 5</div>
+            </div>
+
+            <div class="col-1"></div>
+
+            <div class="col-3 info-box border">
+                <div class="col-2 name-search">Busca 6</div>
+            </div> 
+             
+            <div class="col-12 footer-size"></div>
+
+        </div>    
+        
+    </body>
+
+</html>
